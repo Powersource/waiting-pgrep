b00a6e0a08aef26025ec16324256e520ce08b9a3
Jacob Karlsson <jacob.karlsson95@gmail.com>
2018-09-14 17:11:17 +0200

    Add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+#[macro_use]
 extern crate clap;
 extern crate psutil;
 
 use std::vec::Vec;
 use std::{thread, time::Duration};
 use psutil::process::Process;
+use clap::{App, Arg};
 
 fn main() {
+    let matches = App::new("Waiting pgrep")
+        .version(crate_version!())
+        .arg(Arg::with_name("name")
+            .help("The program name to look for")
+            .required(true)
+            .index(1))
+        .get_matches();
+
+    let name = matches.value_of("name").unwrap().to_string();
+
     loop {
         match psutil::process::all() {
             Err(why) => {
@@ -17,7 +29,7 @@ fn main() {
                 }
             },
             Ok(procs) => {
-                if find_proc("nvim".to_string(), procs) {
+                if find_proc(&name, procs) {
                     // Found and printed. Quitting.
                     return;
                 } else {
@@ -29,9 +41,9 @@ fn main() {
     }
 }
 
-fn find_proc(name: String, procs: Vec<Process>) -> bool {
+fn find_proc(name: &String, procs: Vec<Process>) -> bool {
     let mut found = false;
-    for p in procs.iter().filter(|p| p.comm == name) {
+    for p in procs.iter().filter(|p| &p.comm == name) {
         println!("{}", p.pid);
         found = true;
     }
