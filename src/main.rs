6a55da0921bd9492bcd9397c49b1ca4b7068c1f8
Jacob Karlsson <jacob.karlsson95@gmail.com>
2018-09-14 16:32:22 +0200

    Implement regular pgrep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate clap;
+extern crate psutil;
+
+use std::vec::Vec;
+use psutil::process::Process;
+
 fn main() {
     println!("Hello, world!");
+
+    loop {
+        match psutil::process::all() {
+            Err(why) => {
+                if why.kind() == std::io::ErrorKind::NotFound {
+                    // Supposedly benign error according to the documentation
+                    continue;
+                } else {
+                    panic!("Unknown error {}", why);
+                }
+            },
+            Ok(procs) => {
+                return find_proc("code-oss".to_string(), procs);
+            }
+        }
+    }
+}
+
+fn find_proc(name: String, procs: Vec<Process>) {
+    for p in procs.iter().filter(|p| p.comm == name) {
+        println!("{}", p.pid);
+    }
 }
